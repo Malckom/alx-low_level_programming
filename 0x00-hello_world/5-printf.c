50667f2e98cbdb578aa695d6d2b439d110604fd1
Malcom Nyaranga <kibisu14@icloud.com>
2023-08-11 11:46:21 -0700

    My fourth commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main entry point of the program
+ * @void - Takes no arguments
+ *
+ * Description - prints 'with proper grammar,
+ * but the outcome is a piece of art,' to the screen
+ * Return: returns 0 when successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
