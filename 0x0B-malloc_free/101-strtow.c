9191dd154405ccd189d2b040525da4fa0c83e389
Malcom Nyaranga <kibisu14@icloud.com>
2023-09-05 17:38:47 -0700

    Creating 0x0B-malloc_free variables.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,47 +1,104 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
+
 /**
- * _realloc - reallocates old to new, set conditions from problem
- * returning dest w/ size of malloc new_size, set src as ptr
- * @ptr: pointer to memory prev alloc, must free end
- * @old_size: input old
- * @new_size: input new
- * Return: 0
+ * split_str - handles the string spliting
+ * @s: the string to be splitted
+ * @str_array: memory block allocated to
+ *	store array of strings
+ * Return: returns an array of string
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+
+char **split_str(char *s, char **str_array)
 {
-	char *dest, *src;
-	unsigned int i;
+	int i = 0, j, word_len;
 
-	if (new_size == old_size)
-		return (ptr);
-
-	if (ptr == NULL)
+	while (*s != '\0')
 	{
-		ptr = malloc(new_size);
-		if (ptr == NULL)
+		/* skip leading whitespace */
+		while (*s == ' ')
 		{
-			return (NULL);
+			s++;
 		}
-		return (ptr);
-	}
+		if (*s != '\0')
+		{
+			char *word_start;
 
-	if (new_size == 0 && ptr != NULL)
+			word_start = s;
+			/* find the end of the word */
+			while (*s != ' ' && *s != '\0')
+			{
+				s++;
+			}
+			/* Allocate memory for the word */
+			word_len = s - word_start;
+			str_array[i] = (char *)malloc((word_len + 1) * sizeof(char));
+			if (str_array[i] == NULL)
+			{
+				/* free memory for previously allocated words */
+				for (j = 0; j < i; j++)
+				{
+					free(str_array[j]);
+				}
+				free(str_array);
+				return (NULL);
+			}
+			/* copy the word into the array */
+			strncpy(str_array[i], word_start, word_len);
+			str_array[i][word_len] = '\0';
+			i++;
+		}
+	}
+	return (str_array);
+}
+/**
+ * strtow - splits a string into words
+ * @str: the string to be splitted
+ *
+ * Return: returns NULL if str == NULL, or str == "",
+ *	or error allocating memory, otherwise
+ *	returns a pointer to an array of strings
+ */
+
+char **strtow(char *str)
+{
+	char **str_array;
+	int words_count = 0;
+	char *s;
+
+	if (str == NULL || *str == '\0')
 	{
-		free(ptr);
 		return (NULL);
 	}
-
-	dest = malloc(new_size);
-	if (dest == NULL)
+	s = str;
+	while (*s != '\0')
+	{
+		/* skip leading whitespace */
+		while (*s == ' ')
+		{
+			s++;
+		}
+		if (*s != '\0')
+		{
+			words_count++;
+		}
+		if (*s != ' ' && *s != '\0')
+		{
+			s++;
+		}
+	}
+	if (words_count == 0)
+	{
 		return (NULL);
-
-	src = ptr;
-
-	for (i = 0; i < new_size && i < old_size; i++)
-		dest[i] = src[i];
-	free(ptr);
-
-	return (dest);
+	}
+	str_array = (char **)malloc((words_count + 1) * sizeof(char *));
+	if (str_array == NULL)
+	{
+		return (NULL);
+	}
+	s = str;
+	str_array = split_str(s, str_array);
+	str_array[words_count] = NULL;
+	return (str_array);
 }
